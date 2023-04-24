018ce242585cfa5989aabff7ef033fb5d8180b11
parkdh9018 <parkdh9018@naver.com>
2023-04-24 16:37:34 +0900

    [FEAT] : 폰트 + 노멀라이즈

--- frontend/src/page/NormalPage/main/Main.tsx
+++ frontend/src/page/NormalPage/main/Main.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/button/Button"
+
 export const Main = () => {
   return (
-    <div>Main</div>
+    <>
+    <div style={{fontFamily:"cursive"}}>안녕하세요</div>
+    <div style={{fontSize:"40px", fontWeight: "800"}}>모든 인류 구성원의 천부의 존엄성</div>
+
+    <Button content="안녕하세요" onClick={()=>{}}/>
+    </>
   )
 }
