4ce2f057cebdef1085da62f2bbd8ce194da06f1b
42.4.woonchoi <42.4.woonchoi@gmail.com>
2023-04-24 17:05:29 +0900

    [FEAT] 문제 시간복잡도, 공간복잡도

--- fastapi/processing/processing.py
+++ fastapi/processing/processing.py
@@ -7,22 +7,18 @@ from parser.problem.parse_summary import parse_summary
 
 async def processing(data : ProblemData):
     if (True):
-        chat_llm_0 = ChatOpenAI(temperature=0, openai_api_key=data.openai_api_key)
-        # chat_llm_1 = ChatOpenAI(temperature=0.1, openai_api_key=data.openai_api_key)
-        chat_llm_3 = ChatOpenAI(temperature=0.3, openai_api_key=data.openai_api_key)
-        # chat_llm_10 = ChatOpenAI(temperature=1, openai_api_key=data.openai_api_key)
-
         # 문제 요약 정보 생성 
-        summary_info_result = await summary_info(chat_llm_0, data)
+        summary_info_result = await summary_info(await create_chat_llm(0, data), data)
         print("문제 요약 정보임 : \n", summary_info_result)
 
         # 문제 요약 정보를 참고하여 모범 답안 생성 (시간 복잡도, 공간 복잡도)
-        summary_description_result = await summary_description(chat_llm_0, data, summary_info_result)
+        summary_description_result = await summary_description(await create_chat_llm(0, data), data, summary_info_result)
         print(summary_description_result)
         summary_text = f"problem_id = {data.problemId}\n {summary_info_result} \n {summary_description_result}"
-        summary_json = await parse_summary(chat_llm_0, summary_text)
-        summary_code_complexity_result = await summary_code_complexity(chat_llm_0, data, summary_json)
+        summary_json = await parse_summary(await create_chat_llm(0, data), summary_text)
+        summary_code_complexity_result = await summary_code_complexity(await create_chat_llm(0, data), data, summary_json)
         return summary_code_complexity_result
     
         
-        
+async def create_chat_llm(temperature : int, data : ProblemData):
+    return ChatOpenAI(temperature=0, openai_api_key=data.openai_api_key, request_timeout=3600)
