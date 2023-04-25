769c4d7818048398391ddeee88350e2e3b461679
parkdh9018 <parkdh9018@naver.com>
2023-04-25 15:06:57 +0900

    [FEAT] : 메인 페이지 - 몹 프로그래밍

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
