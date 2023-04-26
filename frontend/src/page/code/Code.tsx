b2a1f6a0b502dba789fa9aa327964c30f3d32aef
parkdh9018 <parkdh9018@naver.com>
2023-04-26 17:45:56 +0900

    [FIX] : 코드 분석 페이지 default 화면 + 글씨 import 수정

--- frontend/src/page/code/Code.tsx
+++ frontend/src/page/code/Code.tsx
@@ -1,15 +1,15 @@
-import style from "./Code.module.css"
-import { SideNav } from "./sideNav/SideNav"
+import style from "./Code.module.css";
+import { SideNav } from "./sideNav/SideNav";
 
 export const Code = () => {
   return (
     <div className={style.code}>
-      <div style={{width: "20%"}}>
-        <SideNav/>
+      <div className={style.leftTab} style={{ width: "20%" }}>
+        <SideNav />
       </div>
-      <div style={{width: "80%", height: "1000px", backgroundColor: "blue"}}>
-
+      <div className={style.rightTab}>
+        <div style={{fontSize: "3rem", color: "gray"}}>ALGOPAT</div>
       </div>
     </div>
-  )
-}
+  );
+};
