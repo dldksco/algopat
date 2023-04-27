d9018ef9447bda2e1c40ce920f400825a2fd09c3
parkdh9018 <parkdh9018@naver.com>
2023-04-27 14:29:16 +0900

    [FEAT] : 코드 분석 페이지 반응형 + 마이페이지 스켈레톤

--- frontend/src/page/code/Code.tsx
+++ frontend/src/page/code/Code.tsx
@@ -1,18 +1,31 @@
 import style from "./Code.module.css";
 import PolynomialChart from "./graph/PolynomialChart";
+import { useState } from "react";
 import { SideNav } from "./sideNav/SideNav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export const Code = () => {
+  const [isSidenavOpen, setIsSidenavOpen] = useState(false);
+
+  const burgerClick = () => setIsSidenavOpen((prev) => !prev);
   return (
     <div className={style.code}>
-      <div className={style.leftTab} style={{ width: "20%" }}>
-        <SideNav />
+      <div
+        className={
+          isSidenavOpen ? style.leftTab + " " + style.mobileon : style.leftTab
+        }
+      >
+        <SideNav setIsSidenavOpen={setIsSidenavOpen} />
       </div>
       <div className={style.rightTab}>
-        <div style={{fontSize: "3rem", color: "gray"}}>ALGOPAT</div>
-        <PolynomialChart></PolynomialChart>
+        <div style={{ fontSize: "3rem", color: "gray" }}>ALGOPAT</div>
+        {/* 뭔가 컴포넌트 <DetailPage/> */}
+        <div className={style.hamberger} onClick={burgerClick}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+        {/* <PolynomialChart /> */}
       </div>
     </div>
   );
 };
-
