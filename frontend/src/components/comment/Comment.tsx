21ce5aee704e7c39d2f021ff8361dd992f5fccd7
YeonHak Lee <dldusgkr788@gmail.com>
2023-05-01 09:58:08 +0900

    [FEAT] 공통 SelectBox 작성 / Comment Component 이동

---
+++ frontend/src/components/comment/Comment.tsx
@@ -0,0 +1,37 @@
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import style from "./Comment.module.css";
+
+interface CommentSet {
+  data: CommentProps;
+}
+
+interface CommentProps {
+  userName: string;
+  tier: number;
+  comment: string;
+  recommend: number;
+}
+
+export const Comment = ({ data }: CommentSet) => {
+  const { userName, tier, comment, recommend } = data;
+  return (
+    <>
+      <div className={style.user}>
+        <img
+          src={`https://static.solved.ac/tier_small/${tier}.svg`}
+          alt="tier"
+        />
+        <span>{userName}</span>
+      </div>
+      <div className={style.comment}>
+        <span>{comment}</span>
+        <div className={style.recommend}>
+          <FontAwesomeIcon icon={faThumbsUp}></FontAwesomeIcon>
+          {recommend}
+        </div>
+      </div>
+    </>
+  );
+};
+3;
---
+++ frontend/src/components/selectBox/SelectBox.tsx
@@ -0,0 +1,53 @@
+import { v4 as uuidv4 } from "uuid";
+import style from "./SelectBox.module.css";
+import { HTMLAttributes, useState } from "react";
+
+interface SelectBoxProps extends HTMLAttributes<HTMLSelectElement> {
+  options: Options[];
+  readonly?: boolean;
+  setValue: (val: string) => void;
+}
+
+interface Options {
+  value: string;
+  name: string;
+}
+
+/** select-box를 사용할때는 꼭 기본값을 주입해 줘야 한다.
+ *  또한 option에 사용될 {value , name} 오브젝트 배열을 주입해 주어야 한다.
+ *  setValue의 값도 주입해 주어야 한다. 부모 컴포넌트에서 useState를 통해서
+ *  select-box에서 선택된 값을 유지한다.
+ */
+export const SelectBox = ({
+  options,
+  defaultValue,
+  readonly,
+  setValue,
+  ...props
+}: SelectBoxProps) => {
+  const [select, setSelect] = useState(defaultValue);
+  const chageValue = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setValue(e.target.value);
+    setSelect(e.target.value);
+  };
+
+  return (
+    <select
+      className={style.select_box}
+      value={select}
+      onChange={(e) => chageValue(e)}
+      {...props}
+    >
+      {options.map((option) => (
+        <option
+          className={style.option}
+          key={uuidv4()}
+          value={option.value}
+          disabled={readonly}
+        >
+          {option.name}
+        </option>
+      ))}
+    </select>
+  );
+};
