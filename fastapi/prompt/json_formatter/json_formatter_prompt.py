93088126d38e93495aae66bea851cb2050b895f4
42.4.woonchoi <42.4.woonchoi@gmail.com>
2023-05-02 09:15:24 +0900

    [FEAT] json 프롬프트 고도화

--- fastapi/prompt/json_formatter/json_formatter_prompt.py
+++ fastapi/prompt/json_formatter/json_formatter_prompt.py
@@ -7,6 +7,8 @@ JSON_FORMATTER_PROMPT_TMPL = (
     "Your output should be in a form that can be parsed by python's json library.\n"
     "A numbered list must hold a number and have it as a string, including newlines.\n"
     "Never arbitrarily transform data. If the incoming data is in the form of a list, store it as text, not a list.\n"
+    "[IMPORTANT]Only two data types are allowed in JSON: str and int.\n"
+    "[IMPORTANT]List-type data is strictly prohibited. Lists should be treated as strings.\n"
     "Only return data in JSON format\n"
     "DATA:\n"
     "--------\n"
