b95f7af678848a8ecb37fbaebb7cebefc1d4ae1b
42.4.woonchoi <42.4.woonchoi@gmail.com>
2023-05-02 17:36:42 +0900

    [FEAT] 데코레이터(로깅) 설정, session close 설정

--- fastapi/database/get_session.py
+++ fastapi/database/get_session.py
@@ -2,10 +2,6 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 import json
 
-from database.problem import insert_problem, insert_gpt_problem_summary, insert_user_submit_problem, check_problem_is_exist
-from myclass.problem import ProblemSummary
-from pydantic import BaseModel
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
@@ -16,7 +12,7 @@ from utils.shared_env import mariadb_config
 
 config = json.loads(mariadb_config)
 
-DATABASE_URL = f"mysql+aiomysql://{config['user']}:{config['password']}@{config['host']}:{config['port']}/{config['database']}?charset=utf8mb4"
+DATABASE_URL = f"mariadb+aiomysql://{config['user']}:{config['password']}@{config['host']}:{config['port']}/{config['database']}?charset=utf8mb4"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
@@ -24,7 +20,7 @@ async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession
 # session 정보 가져오기 
 async def get_session() -> AsyncSession:
     async with async_session() as session:
-        return session
+        yield session
     
 Base = declarative_base()
 async def create_tables():
