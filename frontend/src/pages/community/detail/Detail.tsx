3c61f8c13919d95a2ade5b3c0f00ca32e7bd72ba
YeonHak Lee <dldusgkr788@gmail.com>
2023-05-02 17:34:22 +0900

    [FEAT] community Detail 페이지 작성

--- frontend/src/pages/community/detail/Detail.tsx
+++ frontend/src/pages/community/detail/Detail.tsx
@@ -1,8 +1,79 @@
+import Editor from "@monaco-editor/react";
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Input } from "@/components/input/Input";
+import { useState } from "react";
+import style from "./Detail.module.css";
+import { CommentGroup } from "./commentGroup/CommentGroup";
+
 export const Detail = () => {
-  const dummyData = [
-    { userName: "라면부엉", tier: 1, comment: "아라랄라라라라", recommend: 12 },
-    { userName: "alice", tier: 2, comment: "아라랄라라라라", recommend: 1 },
-    { userName: "Seja", tier: 3, comment: "아라랄라라라라", recommend: 12 },
-  ];
-  return <div>Detail</div>;
+  const dumy2 = "//여기에 코드 더미데이터 들어감\n //되냐?";
+
+  const [input, setInput] = useState("글의 제목");
+  return (
+    <div className={style.detail}>
+      <div className={style.center}>
+        <span>제목</span>
+        <Input
+          input={input}
+          setInput={setInput}
+          disabled={true}
+          style={{
+            backgroundColor: "var(--mypage-box-color)",
+            border: "none",
+            width: "90%",
+          }}
+        />
+      </div>
+      <div className={style.content}>
+        <span>내용</span>
+        <textarea
+          defaultValue={"내용을 여기다가 집어넣는다.\n되냐?"}
+          disabled={true}
+        />
+      </div>
+      <div className={style.center} style={{ marginTop: "20px" }}>
+        <span>언어</span>
+        <Input
+          input={"JAVA"}
+          setInput={setInput}
+          disabled={true}
+          style={{
+            backgroundColor: "var(--mypage-box-color)",
+            border: "none",
+            width: "12%",
+          }}
+        />
+      </div>
+      <div className={style.center} style={{ marginTop: "20px" }}>
+        <span>코드</span>
+        <div
+          style={{
+            position: "relative",
+            top: "-15px",
+            right: "-50px",
+            width: "85%",
+          }}
+        >
+          <Editor
+            height="250px"
+            width="100%"
+            defaultLanguage="javascript"
+            defaultValue={dumy2}
+            theme="vs-dark"
+            options={{
+              insertSpaces: true,
+              tabSize: 2,
+              readOnly: true,
+            }}
+          />
+        </div>
+      </div>
+      <div className={style.recommand}>
+        <FontAwesomeIcon icon={faThumbsUp} />
+        <span>추천하기</span>
+      </div>
+      <CommentGroup />
+    </div>
+  );
 };
