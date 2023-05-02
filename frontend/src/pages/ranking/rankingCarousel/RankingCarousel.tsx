07e2bc1a6cbffc8504b4e213871763ab6a9d12ea
parkdh9018 <parkdh9018@naver.com>
2023-05-02 15:43:30 +0900

    [FEAT] : 랭크 선택바 진행중
    - 중앙에 온 랭크만 크게, 중앙 옆에는 조금만 크게
    - 숫자 셀렉트박스 선택하면 랭크이동 (같은 색 안에서)

--- frontend/src/pages/ranking/rankingCarousel/RankingCarousel.tsx
+++ frontend/src/pages/ranking/rankingCarousel/RankingCarousel.tsx
@@ -1,38 +1,202 @@
-import Slider from "react-slick";
+import Slider, { Settings } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { v4 as uuidv4 } from "uuid";
+import { useEffect, useRef, useState } from "react";
 
 import style from "./RankingCarousel.module.css";
+import "./carousel.css";
+import { SelectBox } from "@/components/selectBox/SelectBox";
+import { Button } from "@/components/button/Button";
 
 export const RankingCarousel = () => {
-  // 더미 데이터 대신에 RQ를 여기서 실행
-  const levelData = Array.from({ length: 31 }, (_, i) => {
+  const initData = Array.from({ length: 31 }, (_, i) => {
     return {
-      level: i,
+      level: i + 1,
+      center: false,
+      left: false,
+      right: false,
     };
   });
 
-  const settings = {
+  const levelNumber = [
+    { value: "0", name: "5" },
+    { value: "1", name: "4" },
+    { value: "2", name: "3" },
+    { value: "3", name: "2" },
+    { value: "4", name: "1" },
+  ];
+
+  const levelRank = [
+    { value: "0", name: "Bronze" },
+    { value: "1", name: "Silver" },
+    { value: "2", name: "Gold" },
+    { value: "3", name: "Platinum" },
+    { value: "4", name: "Diamond" },
+    { value: "5", name: "Ruby" },
+  ];
+
+  const backgroundColorFilter = (rank: number) => {
+    if (1 <= rank && rank < 6) {
+      return "black";
+    } else if (6 <= rank && rank < 11) {
+      return "black";
+    } else if (11 <= rank && rank < 16) {
+      return "black";
+    } else {
+      return "black";
+    }
+  };
+
+  const bj_level = [
+    "Unrated",
+    "Bronze V",
+    "Bronze IV",
+    "Bronze III",
+    "Bronze II",
+    "Bronze I",
+    "Silver V",
+    "Silver IV",
+    "Silver III",
+    "Silver II",
+    "Silver I",
+    "Gold V",
+    "Gold IV",
+    "Gold III",
+    "Gold II",
+    "Gold I",
+    "Platinum V",
+    "Platinum IV",
+    "Platinum III",
+    "Platinum II",
+    "Platinum I",
+    "Diamond V",
+    "Diamond IV",
+    "Diamond III",
+    "Diamond II",
+    "Diamond I",
+    "Ruby V",
+    "Ruby IV",
+    "Ruby III",
+    "Ruby II",
+    "Ruby I",
+    "Master",
+  ];
+
+  const [levelNumberSelect, setlevelNumberSelect] = useState(
+    levelNumber[0].value
+  );
+  const [levelRankSelect, setlevelRankSelect] = useState(levelRank[0].name);
+  const [levelData, setLevelData] = useState(initData);
+  const [centerIndex, setCenterIndex] = useState(0);
+  const sliderRef = useRef<Slider>(null);
+
+  const settings: Settings = {
+    className: style.slider,
     centerPadding: "60px",
     centerMode: true,
     infinite: false,
     speed: 350,
     slidesToShow: 5,
     slidesToScroll: 1,
+    afterChange: (index: number) => {
+      setCenterIndex(index);
+
+      // 초기화
+      setLevelData((prev) =>
+        prev.map((v) => {
+          return { ...v, left: false, right: false, center: false };
+        })
+      );
+
+      // left
+      if (index - 1 >= 0) {
+        setLevelData((prev) => {
+          prev[index - 1].left = true;
+          return [...prev];
+        });
+      }
+
+      // center
+      setLevelData((prev) => {
+        prev[index].center = true;
+        return [...prev];
+      });
+
+      // right
+      if (index + 1 < 31) {
+        setLevelData((prev) => {
+          prev[index + 1].right = true;
+          return [...prev];
+        });
+      }
+    },
+    onInit: () => {
+      const index = centerIndex;
+
+      // center
+      setLevelData((prev) => {
+        prev[index].center = true;
+        return [...prev];
+      });
+
+      // right
+      setLevelData((prev) => {
+        prev[index + 1].right = true;
+        return [...prev];
+      });
+    },
   };
 
+  // 셀렉트 박스 선택에 따라 슬라이드 이동
+  // 숫자 선택
+  useEffect(() => {
+    console.log(centerIndex, levelNumberSelect);
+    if (!sliderRef.current) return;
+    sliderRef.current.slickGoTo(
+      Math.floor(centerIndex / 5) * 5 + Number(levelNumberSelect)
+    );
+  }, [levelNumberSelect]);
+  // 랭크 선택
+  // useEffect(() => {
+  //   // if (!sliderRef.current) return;
+  //   // sliderRef.current.slickGoTo(
+  //   //   Math.floor(centerIndex / 5) * 5 + Number(levelNumberSelect)
+  //   // );
+  // }, [levelRankSelect]);
+
   return (
-    <div className={style.carousel}>
-      <Slider {...settings}>
-        {levelData.map((v) => {
-          return (
-            <div className={style.content} key={uuidv4()}>
-              <img src={`https://static.solved.ac/tier_small/${v.level}.svg`} />
-            </div>
-          );
-        })}
-      </Slider>
+    <div
+      className={style.ranking_carousel}
+      style={{ backgroundColor: `${backgroundColorFilter(centerIndex)}` }}
+    >
+      <div className={style.menu}>
+        <SelectBox options={levelNumber} setValue={setlevelNumberSelect} />
+        <SelectBox options={levelRank} setValue={setlevelRankSelect} />
+      </div>
+      <div className={style.carousel}>
+        <Slider ref={sliderRef} {...settings}>
+          {levelData.map((v) => {
+            return (
+              <div
+                className={
+                  style.content +
+                  " " +
+                  (v.left || v.right ? style.center_side : "") +
+                  " " +
+                  (v.center ? style.center : "")
+                }
+                key={uuidv4()}
+              >
+                <img
+                  src={`https://static.solved.ac/tier_small/${v.level}.svg`}
+                />
+                <p>{bj_level[v.level]}</p>
+              </div>
+            );
+          })}
+        </Slider>
+      </div>
     </div>
   );
 };
