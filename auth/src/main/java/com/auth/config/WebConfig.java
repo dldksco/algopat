a3eef68470fbd0f59809c216a5e3d33dae6a836b
이안채 <dldksco@naver.com>
2023-05-03 11:56:06 +0900

    [FEAT]CORS

--- auth/src/main/java/com/auth/config/WebConfig.java
+++ auth/src/main/java/com/auth/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.auth.config;
 
-import com.auth.filter.CustomCorsFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
