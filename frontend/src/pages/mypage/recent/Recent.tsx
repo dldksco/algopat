af66b7650965c8c79be0180414ba02a396042246
김소현 <alice2596@naver.com>
2023-05-04 14:54:51 +0900

    feat(CheckBox.tsx): Checkbox 기능 추가

--- frontend/src/pages/mypage/recent/Recent.tsx
+++ frontend/src/pages/mypage/recent/Recent.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import { Pagenation } from "@/components/pagenation/Pagenation";
 import { Outlet, useNavigate } from "react-router-dom";
-import {useSetRecoilState} from "recoil";
+import { useSetRecoilState } from "recoil";
 import { prevSelectedState } from "@/atoms/selected.atom";
 import style from "./Recent.module.css";
 import { MenuBar } from "./menubar/MenuBar";
 
 export const Recent = () => {
   const setPrevSelected = useSetRecoilState(prevSelectedState);
-  
+
   // bar 위치 초기값
   useEffect(() => {
     setPrevSelected("text");
@@ -18,12 +18,10 @@ export const Recent = () => {
     <>
       <div className={style.box}>
         <div className={style.recentTitle}>최근 활동</div>
-        <MenuBar/>
+        <MenuBar />
         <div>
-        <Outlet />
-
+          <Outlet />
         </div>
-        <Pagenation first={false} last={false} number={3} totalPages={32} />
       </div>
     </>
   );
