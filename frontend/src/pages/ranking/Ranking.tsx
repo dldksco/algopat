c4f11d1a84f0e41264764c0779bc0b1c638211dc
parkdh9018 <parkdh9018@naver.com>
2023-05-04 14:57:17 +0900

    [FIX] : 랭킹 라우터 수정

--- frontend/src/pages/ranking/Ranking.tsx
+++ frontend/src/pages/ranking/Ranking.tsx
@@ -1,10 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { RankingCarousel } from "./rankingCarousel/RankingCarousel";
+import { RankingBoard } from "./rankingBoard/RankingBoard";
+import { Pagenation } from "@/components/pagenation/Pagenation";
+
 import style from "./Ranking.module.css";
 
 export const Ranking = () => {
   return (
     <>
-      <Outlet />
+      <div className={style.RankingMain}>
+        <RankingCarousel />
+        <RankingBoard />
+        <Pagenation first={true} last={true} />
+      </div>
     </>
   );
 };
