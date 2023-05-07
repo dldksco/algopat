c2eec068d3433d16e6290413ad079ccd02d8d612
이찬희 <dojsfffff@naver.com>
2023-05-07 17:34:31 +0900

    [FEAT] 회원푼문제 임시 구현

---
+++ code/src/main/java/com/code/data/repository/UserSubmitProblemRepository.java
@@ -0,0 +1,8 @@
+package com.code.data.repository;
+
+import com.code.data.entity.UserSubmitProblem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserSubmitProblemRepository extends JpaRepository<UserSubmitProblem, Long> {
+
+}
