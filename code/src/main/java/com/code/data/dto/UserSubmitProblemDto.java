474b49735348c0e94b5c50c356a641037c895f29
이찬희 <dojsfffff@naver.com>
2023-05-07 16:10:16 +0900

    [FEAT] 날짜 데이터 관리 패키지 생성

---
+++ code/src/main/java/com/code/data/dto/UserSubmitProblemDto.java
@@ -0,0 +1,15 @@
+//package com.code.data.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
+//public class UserSubmitProblemDto {
+//  private long problemId;
+//  private long userSeq;
+//  private String problemTitle;
+//  private long problemLevel;
+//  private List<String> submissionId; // Todo : String 타입이 아닌, 제출 번호 + 제출 일자 또는 업데이트 일자 담는 DTO 타입으로 변경
+//
+//}
