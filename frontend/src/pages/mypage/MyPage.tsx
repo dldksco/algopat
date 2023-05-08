d213140fa4f834ebbb57956fe8bc7eede0c96cdf
김소현 <alice2596@naver.com>
2023-05-08 13:04:09 +0900

    fix(MyPage.tsx): 그리드 스켈레톤 코드 및 마이페이지 수정

--- frontend/src/pages/mypage/MyPage.tsx
+++ frontend/src/pages/mypage/MyPage.tsx
@@ -1,14 +1,10 @@
 import style from "./MyPage.module.css";
-import { Activity} from "./activity/Activity";
 import { Outlet } from "react-router-dom";
-
 
 export const MyPage = () => {
   return (
     <>
       <div className={style.mypage}>
-        <div className={style.buffer}></div>
-        <Activity/>
         <Outlet />
         <div style={{ height: "5%" }}></div>
       </div>
--- frontend/src/pages/mypage/profile/grid/Grid.tsx
+++ frontend/src/pages/mypage/profile/grid/Grid.tsx
@@ -1,26 +1,68 @@
 import style from "./Grid.module.css";
+import type { Properties } from "csstype";
 
-export const Grid = () => {
-
-    const days =["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"];
-    const months =["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug","Sep", "Oct", "Nov", "Dec"];
-
-    const items = [];
-    for(let i = 1; i <= 366; i++) {
-    items.push({
-        id: i,
-        name: `${i}`,
-        month: `${months[i%7]}`,
-        day: `${days[i%7]}`
-    });
-    }
-    return (
-        <>
-        <div className={style.gridcontainer}>
-        {items.map(item => (
-        <div key={item.id} className={style.grid}></div>
-      ))}
-        </div>
-    </>
-    )
+interface GridProps {
+  startColumn: number;
+  grassColor: number[];
 }
+
+type TableStyles = {
+  borderCollapse: Properties["borderCollapse"];
+  borderSpacing: string;
+};
+
+export const Grid = (props: GridProps) => {
+  const { grassColor } = props;
+  const greenColors = ["FFFFFF", "#008000", "#00FF00", "#32CD32", "#90EE90"];
+  let index = 0;
+  const rows = [];
+  for (let row = 0; row < 7; row++) {
+    const cells = [];
+    for (let col = 0; col < 53; col++) {
+      const value = grassColor[row * 53 + col];
+      if (value == 0) {
+        index = 0;
+      } else if (value <= 2) {
+        index = 1;
+      } else if (value <= 4) {
+        index = 2;
+      } else {
+        index = 3;
+      }
+      const cellStyle = {
+        backgroundColor: greenColors[index],
+      };
+      cells.push(<td key={col} className={style.cell} style={cellStyle} />);
+    }
+    rows.push(<tr key={row}>{cells}</tr>);
+  }
+  const tableStyles: TableStyles = {
+    borderCollapse: "separate", // separate the borders of each cell
+    borderSpacing: "10px", // add a gap of 10px between each cell
+  };
+
+  //   const days = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"];
+  //   const months = [
+  //     "Jan",
+  //     "Feb",
+  //     "Mar",
+  //     "Apr",
+  //     "May",
+  //     "Jun",
+  //     "Jul",
+  //     "Aug",
+  //     "Sep",
+  //     "Oct",
+  //     "Nov",
+  //     "Dec",
+  //   ];
+  return (
+    <>
+      <div className={style.container}>
+        <table className={style.table}>
+          <tbody>{rows}</tbody>
+        </table>
+      </div>
+    </>
+  );
+};
