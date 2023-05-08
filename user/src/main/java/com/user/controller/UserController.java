e44022a1f58751a92eb0158e34181619abeb91cc
이안채 <dldksco@naver.com>
2023-05-08 14:11:37 +0900

    [FEAT]API GATE WAY HEADER TEST

--- user/src/main/java/com/user/controller/UserController.java
+++ user/src/main/java/com/user/controller/UserController.java
@@ -4,11 +4,13 @@ import com.user.dto.GithubUserIdInfoDTO;
 import com.user.repository.UserRepository;
 import com.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
   private final UserService userService;
   @PostMapping("/check")
-  public ResponseEntity<Boolean> userCheck(@RequestBody GithubUserIdInfoDTO githubUserIdInfoDTO) {
-
+  public ResponseEntity<Boolean> userCheck(@RequestBody GithubUserIdInfoDTO githubUserIdInfoDTO,@RequestHeader HttpHeaders headers) {
+    System.out.println();
     return new ResponseEntity<>(userService.userCheck(githubUserIdInfoDTO), HttpStatus.OK);
   }
 }
