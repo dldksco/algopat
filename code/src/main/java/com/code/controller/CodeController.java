5663d35bdce5deeff631fdc0ff2659acb662da3d
이찬희 <dojsfffff@naver.com>
2023-05-09 15:24:18 +0900

    [FIX] 헤더에서 값을 꺼내는 로직 추가

--- code/src/main/java/com/code/controller/CodeController.java
+++ code/src/main/java/com/code/controller/CodeController.java
@@ -10,12 +10,15 @@ import com.code.service.ProblemRankService;
 import com.code.service.ProblemService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/problem")
 public class CodeController {
+
+  // logger 정의
+  private static final Logger logger = LoggerFactory.getLogger(CodeController.class);
 
   private final String USER_CODE_TOPIC = "usercode";
 
@@ -53,7 +59,11 @@ public class CodeController {
   @GetMapping("/submission/{pageNumber}")
   public ResponseEntity<Page<UserSubmitProblemDto>> getUserSubmitProblemDto(
       @PathVariable("pageNumber") int pageNumber,
-      @RequestParam(value = "userSeq", defaultValue = "9999") long userSeq) {
+      @RequestHeader("userSeq") long userSeq
+      ) {
+    logger.info("헤더에서 userSeq 꺼냄 : {}", userSeq);
+
+    userSeq = 9999;
     return ResponseEntity.ok(problemService.getUserSubmitProblemDtoPage(pageNumber, userSeq));
   }
 
