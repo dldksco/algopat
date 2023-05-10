e98f9f14bade33216c9c818732c443f750e29d22
이찬희 <dojsfffff@naver.com>
2023-05-10 13:08:35 +0900

    [FIX] 카프카 DTO 수정

--- fastapi/my_dto/common_dto.py
+++ fastapi/my_dto/common_dto.py
@@ -1,14 +1,6 @@
-
+from pydantic import BaseModel
 # Kafka -> Alert  DTO 
-class MessageDTO:
-    def __init__(self, progress: str, payload: dict, user_seq : int):
-        self.progress = progress
-        self.payload = payload
-        self.user_seq = user_seq
-
-    def to_dict(self):
-        return {
-            "progress": self.progress, # 메소드 이름 
-            "payload": self.payload, # 메시지 
-            "user_seq": self.user_seq # 유저 식별번호 
-        }
+class MessageDTO(BaseModel):
+    progress : str 
+    message  : str 
+    user_seq : int 
