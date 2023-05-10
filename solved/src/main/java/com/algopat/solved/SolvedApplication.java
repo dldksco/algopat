3e60e6b151f8e9f01416d712c335db6468909303
42.4.woonchoi <42.4.woonchoi@gmail.com>
2023-05-10 14:21:09 +0900

    [FEAT] 랭크 컨트롤러 분리

---
+++ solved/src/main/java/com/algopat/solved/SolvedApplication.java
@@ -0,0 +1,13 @@
+package com.algopat.solved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvedApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SolvedApplication.class, args);
+  }
+
+}
---
+++ solved/src/test/java/com/algopat/solved/SolvedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algopat.solved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvedApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
