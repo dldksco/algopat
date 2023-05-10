da0b3e9b00a025ad43b49056011f8ee05d05f6e5
42.4.woonchoi <42.4.woonchoi@gmail.com>
2023-05-10 12:20:13 +0900

    [FEAT] 로그 설정

--- fastapi/utils/log_decorator.py
+++ fastapi/utils/log_decorator.py
@@ -1,6 +1,31 @@
 import functools
-from logging import getLogger
 import time
+import os
+from logging import getLogger, FileHandler, Formatter, DEBUG, INFO
+
+# logger 설정 
+logger = getLogger()
+logger.setLevel(DEBUG)
+
+# 로그 파일 저장 경로 설정
+log_directory = "/home/ubuntu/logs"
+os.makedirs(log_directory, exist_ok=True)
+
+# 로그 파일 이름을 컨테이너별로 다르게 설정해야 합니다.
+# 아래와 같이 컨테이너 이름을 가져올 수 있습니다.
+container_name = os.environ.get('CONTAINER_NAME', 'default_container_name')
+log_file_path = os.path.join(log_directory, f"{container_name}.log")
+
+# 로그 파일 핸들러 추가
+file_handler = FileHandler(log_file_path)
+file_handler.setLevel(INFO)
+
+# 로그 메시지 포맷 설정
+formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# 파일 핸들러를 로거에 추가
+logger.addHandler(file_handler)
 
 # logger 설정 
 logger = getLogger()
