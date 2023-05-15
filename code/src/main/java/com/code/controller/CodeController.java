82f0992d063a6f489b067cb2eeb60323ab7457e7
이안채 <dldksco@naver.com>
2023-05-15 17:15:59 +0900

    [FEAT] kafka 설정 변경

--- code/src/main/java/com/code/controller/CodeController.java
+++ code/src/main/java/com/code/controller/CodeController.java
@@ -62,7 +62,7 @@ public class CodeController {
     problemService.checkExistUserSubmitSolution(Long.parseLong(problemRequestDto.getSubmissionId())); // 이미 제출한 코드가 있는지 체크 (있다면 409)
     problemRequestDto.setUserSeq(userSeq);
     kafkaProducerService.send(USER_CODE_TOPIC, problemRequestDto);
-    if(!problemRequestDto.getOpenaiApiKey().equals("0")){
+    if(problemRequestDto.getOpenaiApiKey().equals("0")){
       kafkaProducerService.sendUserSubmitTransactionDto(USER_SERVICE_TOPIC, UserSubmitTransactionDto.builder()
           .userSeq(userSeq)
           .build());
