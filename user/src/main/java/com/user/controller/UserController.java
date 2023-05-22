be7d9ec48d3fa0f7fcd0204cf22de0fbed3eeb4a
이안채 <dldksco@naver.com>
2023-05-22 12:31:48 +0900

    [CHORE]user server 주석

--- user/src/main/java/com/user/controller/UserController.java
+++ user/src/main/java/com/user/controller/UserController.java
@@ -24,17 +24,36 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class UserController {
   private final UserService userService;
+
+  /**
+   * github id를 통해 userSeq를 리턴해줍니다
+   * @author Lee an chae
+   * @param githubUserIdInfoDTO
+   * @return
+   */
   @PostMapping("/check/github-id")
   public ResponseEntity<UserCheckResponseDTO> checkAndJoinGithubUser(@RequestBody GithubUserIdInfoDTO githubUserIdInfoDTO) {
     return new ResponseEntity<>(userService.checkAndJoinGithubUser(githubUserIdInfoDTO), HttpStatus.OK);
   }
 
+  /**
+   * 백준 아이디가 서비스에 가입되었는지 확인하고 가입하지 않았을 시 가입시킵니다.
+   * @author Lee an chae
+   * @param backjoonUserDTO
+   * @return
+   */
   @PostMapping("/check/backjoon-id")
   public ResponseEntity<Void> checkAndJoinBackjoonUser(@RequestBody BackjoonUserDTO backjoonUserDTO) {
     userService.checkAndJoinBackjoonUser(backjoonUserDTO);
     return ResponseEntity.ok().build();
   }
 
+  /**
+   * User 제출 횟수가 몇 회 남았는지 알려줍니다
+   * @author Lee an chae
+   * @param userSeq
+   * @return
+   */
   @GetMapping("/check/user-submit-count")
   public ResponseEntity<UserSubmitCountDTO> checkUserSubmitCount(@RequestHeader("userSeq") long userSeq){
     UserSubmitCountDTO userSubmitCountDTO=userService.findUserSubmitCount(userSeq);
@@ -42,6 +61,12 @@ public class UserController {
     return new ResponseEntity<>(userSubmitCountDTO,HttpStatus.OK);
   }
 
+  /**
+   * 유저 프로필 정보를 반환합니다
+   * @author Lee an chae
+   * @param userSeq
+   * @return
+   */
   @GetMapping("/profile")
   public ResponseEntity<UserInfo> userProfile(@RequestHeader("userSeq") long userSeq) {
     UserInfo userInfo = userService.userProfile(userSeq);
--- user/src/main/java/com/user/service/UserService.java
+++ user/src/main/java/com/user/service/UserService.java
@@ -9,16 +9,54 @@ import com.user.dto.UserSubmitCountDTO;
 import com.user.dto.UserTransactionDTO;
 
 public interface UserService {
+  /**
+   * 깃허브 아이디를 통해 우리 서비스에 가입했는지 확인 후, 안했다면 가입합니다.
+   * @author Lee an chae
+   * @param githubUserIdInfoDTO
+   * @return
+   */
   public UserCheckResponseDTO checkAndJoinGithubUser(GithubUserIdInfoDTO githubUserIdInfoDTO);
+  /**
+   * 회원등록
+   * @author Lee an chae
+   * @param githubUserIdInfoDTO
+   * @return
+   */
   public long joinGithubUser(GithubUserIdInfoDTO githubUserIdInfoDTO);
+  /**
+   * 백준 아이디가 등록되었는지 확인 후, 등록되지 않았다면 등록합니다
+   * @author Lee an chae
+   * @param backjoonUserDTO
+   */
   public void checkAndJoinBackjoonUser(BackjoonUserDTO backjoonUserDTO);
 
-
+  /**
+   * user profile을 return 합니다
+   * @author Lee an chae
+   * @param userSeq
+   * @return
+   */
   public UserInfo userProfile(Long userSeq);
 
+  /**
+   * user 문제 제출 횟수를 증가시킵니다.
+   * @author Lee an chae
+   * @param userTransactionDTO
+   */
   public void plusUserSubmitCount(UserTransactionDTO userTransactionDTO);
 
+  /**
+   * user 문제 제출 횟수를 감소시킵니다.
+   * @author Lee an chae
+   * @param userTransactionDTO
+   */
   public void minusUserSubmitCount(UserTransactionDTO userTransactionDTO);
+  /**
+   * 유저 문제 제출 횟수를 리턴합니다.
+   * @author Lee an chae
+   * @param userSeq
+   * @return
+   */
 
   public UserSubmitCountDTO findUserSubmitCount(Long userSeq);
 
