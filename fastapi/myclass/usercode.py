d02478db5547f7ebf50cad9c6cc2bad8538a0c7c
42.4.woonchoi <42.4.woonchoi@gmail.com>
2023-05-24 08:59:07 +0900

    [FEAT] 프롬프트 수정

--- fastapi/myclass/usercode.py
+++ fastapi/myclass/usercode.py
@@ -3,17 +3,17 @@ from typing import Optional
 
 
 class UsercodeSummary(BaseModel):
+    gpt_solution_time_complexity: Optional[str] = None
     gpt_solution_time_complexity_reason: Optional[str] = None
     gpt_solution_time_complexity_good_point: Optional[str] = None
     gpt_solution_time_complexity_bad_point: Optional[str] = None
     gpt_improving_time_complexity_suggestion: Optional[str] = None
-    gpt_solution_time_complexity: Optional[str] = None
     gpt_solution_time_score: Optional[int] = None
+    gpt_solution_space_complexity: Optional[str] = None
     gpt_solution_space_complexity_reason: Optional[str] = None
     gpt_solution_space_complexity_good_point: Optional[str] = None
     gpt_solution_space_complexity_bad_point: Optional[str] = None
     gpt_improving_space_complexity_suggestion: Optional[str] = None
-    gpt_solution_space_complexity: Optional[str] = None
     gpt_solution_space_score: Optional[int] = None
     gpt_solution_refactoring_suggestion: Optional[str] = None
     gpt_solution_clean_score: Optional[int] = None
